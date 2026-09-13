96593bcb22a26f0127e889fa6b52b1788e5baa4f
Gio Esquivel <gioboyesquivel123@gmail.com>
2026-09-13 19:09:05 +0800

    Create if_Statemens.py

---
+++ if_Statemens.py
@@ -0,0 +1,12 @@
+temperature = float(input("Enter the temp: "))
+if temperature > 35:
+    print("Its a hot day")
+    print("Drink plenty of water")
+elif temperature > 10:
+    print("It's a nice day")
+elif temperature > 100:
+    print("Its a cold day")
+else:
+    print("Its cold")
+print("Done")
+
